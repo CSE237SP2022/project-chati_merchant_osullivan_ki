d91ac8e80efbf10380d39cdd6a2c7c0babce1b92
prathameshchati <chati.p@wustl.edu>
2022-03-27 18:43:21 -0500

    read input edited

--- src/compareDNA/runCompareDNA.java
+++ src/compareDNA/runCompareDNA.java
@@ -1,12 +1,23 @@
 package compareDNA;
+import java.io.*; //read file inputs and generate outputs
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 public class runCompareDNA {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException { //IOException added
 		
-		//accept test files
+//		File dnaSequences = new File("src\\testFiles\\sameRandomDNASequences.txt");
 		
+		Set<String> dnaSequences = new HashSet<>(Files.readAllLines(Paths.get("src/testFiles/sameRandomDNASequences.txt")));
+		
+//		BufferedReader br = new BufferedReader(new FileReader(dnaSequences));
+//		
+//		 String st;
+//	     while ((st = br.readLine()) != null)
+//	    	 System.out.println(st);
 
 	}
 
