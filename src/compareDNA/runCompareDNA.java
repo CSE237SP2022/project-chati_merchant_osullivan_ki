97bdcb337d2a6d3708812f7cf62e493109850f7b
prathameshchati <chati.p@wustl.edu>
2022-03-27 18:16:33 -0500

    compareDNA changed

--- src/compareDNA/runCompareDNA.java
+++ src/compareDNA/runCompareDNA.java
@@ -1,10 +1,13 @@
 package compareDNA;
-import java.io.*; //imported to read inputs and provide outputs
 
 public class runCompareDNA {
 
 	public static void main(String[] args) {
-		File dnaSequences = new File("src\testFiles\test.txt");
+		// TODO Auto-generated method stub
+		
+		//accept test files
+		
+
 	}
 
 }
