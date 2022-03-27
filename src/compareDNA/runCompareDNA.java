2f41b4973167ad38da13eefd0e90b4c487580895
prathameshchati <chati.p@wustl.edu>
2022-03-27 18:53:02 -0500

    read input modified - file reading in properly, need to parse

--- src/compareDNA/runCompareDNA.java
+++ src/compareDNA/runCompareDNA.java
@@ -11,7 +11,16 @@ public class runCompareDNA {
 		
 //		File dnaSequences = new File("src\\testFiles\\sameRandomDNASequences.txt");
 		
-		Set<String> dnaSequences = new HashSet<>(Files.readAllLines(Paths.get("src/testFiles/sameRandomDNASequences.txt")));
+//		Set<String> dnaSequences = new HashSet<>(Files.readAllLines(Paths.get("src/testFiles/sameRandomDNASequences.txt")));
+		File currentDir = new File(".");
+		File parentDir = currentDir.getParentFile();
+		File dnaSequences = new File(parentDir,"testFiles/sameRandomDNASequences.txt");
+		 
+		if(dnaSequences.canRead())
+		{
+		    System.out.println(dnaSequences);
+		}
+//		Set<String> dictionary = new HashSet<>(Files.readAllLines(Paths.get(dnaSequences)));
 		
 //		BufferedReader br = new BufferedReader(new FileReader(dnaSequences));
 //		
