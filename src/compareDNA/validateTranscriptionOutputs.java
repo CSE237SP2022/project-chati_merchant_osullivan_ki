5e68761398373bbc3c61ebef0eb8e76bfff7be51
Josh-Ki <71088619+Josh-Ki@users.noreply.github.com>
2022-03-29 11:46:11 -0500

    Update: prints strand if successful for now and changed to Upper case

--- src/compareDNA/validateTranscriptionOutputs.java
+++ src/compareDNA/validateTranscriptionOutputs.java
@@ -6,15 +6,19 @@ public class validateTranscriptionOutputs {
         int success = 0;
         int isDNA = 1;
         int notDNARNA = 2;
-
-        if (x.contains("t")) { //Checks if the "mRNA has a t or not." Not mRNA if there is a T
-            if (x.contains("u")) { //if there is a U, then it's not DNA. So return not dna or rna
+        String [] sentence = new String [x.length()];
+        for(int i = 0; i < x.length(); i++){
+            sentence[i] = String.valueOf(x.charAt(i));
+        }
+        if (x.contains("T")) { //Checks if the "mRNA has a t or not." Not mRNA if there is a T
+            if (x.contains("U")) { //if there is a U, then it's not DNA. So return not dna or rna
                 return notDNARNA;
             } else { //if there is no U but has a T, then it is still DNA
                 return isDNA;
             }
 
         }
+        System.out.println(sentence);
         return success; //return a success if there is no t or u in the mRNA passed into this.
     }
 }
