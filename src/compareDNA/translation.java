12851211dd71738792900dc651329c268510caad
Josh-Ki <71088619+Josh-Ki@users.noreply.github.com>
2022-03-29 21:30:30 -0500

    Translation edit

--- src/compareDNA/translation.java
+++ src/compareDNA/translation.java
@@ -1,142 +1,67 @@
 package compareDNA;
 
-public class translation {
-    public String[] translater(String x) {
-        int aminosLength = (int) (x.length() / 3);
-        int counter = 0;
-        String[] allAminos = new String[aminosLength];
-        while (counter < aminosLength) {
-        for (int i = 0; i < x.length(); i = i + 3) {
-                // if statements for now, mapping in future.
-                if (x.charAt(i) == 'G') { // If the first is G.
-                    if (x.charAt(i + 1) == 'U') {
-                        allAminos[counter] = "Valine";
-                        counter++;
-                    }
-                    if (x.charAt(i + 1) == 'C') {
-                        allAminos[counter] = "Alanine";
-                        counter++;
-                    }
-                    if (x.charAt(i + 1) == 'A') {
-                        if (x.charAt(i + 2) == 'U' || x.charAt(i + 2) == 'C') {
-                            allAminos[counter] = "Aspartic acid";
-                            counter++;
-                        }
-                        if (x.charAt(i + 2) == 'A' || x.charAt(i + 2) == 'G') {
-                            allAminos[counter] = "Glutamic acid";
-                            counter++;
-                        }
-                    }
-                    if (x.charAt(i + 1) == 'G') {
-                        allAminos[counter] = "Glycine";
-                        counter++;
-                    }
-                }
-                if (x.charAt(i) == 'U') { //If the first letter is U
-                    if (x.charAt(i + 1) == 'U') {
-                        if (x.charAt(i + 2) == 'U' || x.charAt(i + 2) == 'C') {
-                            allAminos[counter] = "Phenylalanine";
-                            counter++;
-                        }
-                        if (x.charAt(i + 2) == 'A' || x.charAt(i + 2) == 'G') {
-                            allAminos[counter] = "Leucine";
-                            counter++;
-                        }
-                    }
-                    if (x.charAt(i + 1) == 'C') {
-                        allAminos[counter] = "Serine";
-                        counter++;
-                    }
+import java.io.*; //read file inputs and generate outputs
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
-                    if (x.charAt(i + 1) == 'A') {
-                            if (x.charAt(i + 2) == 'U' || x.charAt(i + 2) == 'C') {
-                                allAminos[counter] = "Tyrosine";
-                                counter++;
-                            }
-                            if (x.charAt(i + 2) == 'A' || x.charAt(i + 2) == 'G') {
-                                allAminos[counter] = "Stop";
-                                counter++;
-                            }
-                        }
-                        if (x.charAt(i + 1) == 'G') {
-                            if (x.charAt(i + 2) == 'U' || x.charAt(i + 2) == 'C') {
-                                allAminos[counter] = "Cysteine";
-                                counter++;
-                            }
-                            if (x.charAt(i + 2) == 'A') {
-                                allAminos[counter] = "Stop";
-                                counter++;
-                            }
-                            if (x.charAt(i + 2) == 'G') {
-                                allAminos[counter] = "Tryptophan";
-                                counter++;
-                            }
-                        }
-                    }
-                    if (x.charAt(i) == 'C') { // If the first is G.
-                        if (x.charAt(i + 1) == 'U') {
-                            allAminos[counter] = "Leucine";
-                            counter++;
-                        }
-                        if (x.charAt(i + 1) == 'C') {
-                            allAminos[counter] = "Proline";
-                            counter++;
-                        }
-                        if (x.charAt(i + 1) == 'A') {
-                            if (x.charAt(i + 2) == 'U' || x.charAt(i + 2) == 'C') {
-                                allAminos[counter] = "Histidine";
-                                counter++;
-                            }
-                            if (x.charAt(i + 2) == 'A' || x.charAt(i + 2) == 'G') {
-                                allAminos[counter] = "Glutamine";
-                                counter++;
-                            }
-                        }
-                        if (x.charAt(i + 1) == 'G') {
-                            allAminos[counter] = "Arginine";
-                            counter++;
-                        }
-                    }
-                    if (x.charAt(i) == 'A') { // If the first is G.
-                        if (x.charAt(i + 1) == 'C') {
-                            allAminos[counter] = "Threonine";
-                            counter++;
-                        }
-                        if (x.charAt(i + 1) == 'U') {
-                            if (x.charAt(i + 2) == 'G') {
-                                allAminos[counter] = "Methionine";
-                                counter++;
-                            }
-                            if (x.charAt(i + 2) == 'A' || x.charAt(i + 2) == 'U'|| x.charAt(i + 2) == 'C') {
-                                allAminos[counter] = "Isoleucine";
-                                counter++;
-                            }
-                        }
-                        if (x.charAt(i + 1) == 'A') {
-                            if (x.charAt(i + 2) == 'U' || x.charAt(i + 2) == 'C') {
-                                allAminos[counter] = "Asparagine";
-                                counter++;
-                            }
-                            if (x.charAt(i + 2) == 'A' || x.charAt(i + 2) == 'G') {
-                                allAminos[counter] = "Lysine";
-                                counter++;
-                            }
-                        }
-                        if (x.charAt(i + 1) == 'G') {
-                            if (x.charAt(i + 2) == 'U' || x.charAt(i + 2) == 'C') {
-                                allAminos[counter] = "Serine";
-                                counter++;
-                            }
-                            if (x.charAt(i + 2) == 'A' || x.charAt(i + 2) == 'G') {
-                                allAminos[counter] = "Arginine";
-                                counter++;
-                            }
-                        }
+public class translation {
+    public ArrayList<String> translater(String[] dnaSequencesArray) throws FileNotFoundException {
+
+     //   ArrayList<String> dnaSequencesArray = dnaSequencesArray;
+        
+        List<Map.Entry<String, String>> codonAminoAcidMap = new ArrayList<>();
+        Scanner aminoAcidsDictionary = new Scanner(new File("compareDNA/aminoAcidsDictionary.txt")); // CHANGE this to
+                                                                                                     // run from src
+        while (aminoAcidsDictionary.hasNextLine()) {
+            String codonAminoAcidMapping = aminoAcidsDictionary.nextLine(); // store each next dna sequence in the array
+            String[] codonToAminoAcidArray = codonAminoAcidMapping.split("\\s+");
+            String codon = codonToAminoAcidArray[0];
+            String aminoAcid = codonToAminoAcidArray[2];
+            Map.Entry<String, String> codonToAminoAcidEntry = new AbstractMap.SimpleEntry<>(codon, aminoAcid);
+            codonAminoAcidMap.add(codonToAminoAcidEntry);
+        }
+
+        ArrayList<String> aminoAcidSequencesStringArray = new ArrayList<String>();
+
+        for (String dnaSequence : dnaSequencesArray) {
+            List<String> dnaCodonsArray = new ArrayList<String>();
+            for (int i = 0; i < dnaSequence.length(); i += 3) {
+                String newTestLine = dnaSequence.substring(i);
+                String codon = newTestLine.substring(0, 3);
+                dnaCodonsArray.add(codon);
+            }
+
+            ArrayList<String> aminoAcidSequencesCharArray = new ArrayList<String>();
+
+            for (String codon : dnaCodonsArray) {
+                for (Map.Entry<String, String> codonEntry : codonAminoAcidMap) {
+                    String mRNACodonEntry = codonEntry.getKey().replace('T', 'U');
+                    if (mRNACodonEntry.equals(codon)) {
+                        aminoAcidSequencesCharArray.add(codonEntry.getValue());
                     }
                 }
-            
+            }
+
+            StringBuilder aminoAcidPeptideChainBuilder = new StringBuilder();
+
+            for (String aminoAcid : aminoAcidSequencesCharArray) {
+                aminoAcidPeptideChainBuilder.append(aminoAcid);
+            }
+
+            String aminoAcidPeptideChain = aminoAcidPeptideChainBuilder.toString();
+            aminoAcidSequencesStringArray.add(aminoAcidPeptideChain);
+
+            // System.out.println(aminoAcidPeptideChain);
         }
-        return allAminos;
-    
-}
+        return aminoAcidSequencesStringArray;
+    }
 }
